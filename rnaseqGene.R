2b51883f25d34edca66e2afde552f96acb971166
Islam Hamada <islamsalah@gmail.com>
2023-04-18 08:36:53 +0200

    DE commit

--- rnaseqGene.R
+++ rnaseqGene.R
@@ -1,46 +1,89 @@
-# BiocManager::install("rnaseqGene")
 library(rnaseqGene)
-library("airway")
+
 dir <- system.file("extdata", package="airway", mustWork=TRUE)
 list.files(dir)
-list.files(file.path(dir, "quants"))
-
 csvfile <- file.path(dir, "sample_table.csv")
 coldata <- read.csv(csvfile, row.names=1, stringsAsFactors=FALSE)
 coldata
 
-coldata <- coldata[1:2,]
 coldata$names <- coldata$Run
-coldata$files <- file.path(dir, "quants", coldata$names, "quant.sf.gz")
+dir <- "/home/islam/airway"
+coldata$files <- file.path(dir, "quants", paste0(coldata$names,"_quant"), "quant.sf")
 file.exists(coldata$files)
 
-library("tximeta")
-se <- tximeta(coldata)
-dim(se)
-head(rownames(se))
+# makeLinkedTxome
+# check the latest genomes versions
+hashfile <- file.path(system.file("extdata",package="tximeta"),"hashtable.csv")
+hashtable <- read.csv(hashfile,stringsAsFactors=FALSE)
+hashtable[,"organism"]
+
+se <- tximeta(coldata,useHub=FALSE)
 
 gse <- summarizeToGene(se)
 
-
-rowRanges(gse)
-
-colData(gse)
-
+assay(gse)
 assayNames(gse)
 head(assay(gse), 3)
 
-gse$dex
+colSums(assay(gse))
+
+rowRanges(gse)
+
+seqinfo(rowRanges(gse))
+
+colData(gse)
 
 gse$dex <- factor(gse$dex, levels=c("untrt","trt"))
+gse$cell <- factor(gse$cell, unique(gse$cell))
 
-levels(gse$dex)
+levels(gse$cell)
 
-gse$dex %<>% relevel("untrt")
-gse$dex
+dds <- DESeqDataSet(gse, design = ~ cell + dex)
 
-gse$dex <- relevel(gse$dex, "untrt")
+keep <- rowSums(counts(dds)) > 1
+dds <- dds[keep,]
+nrow(dds)
+
+lambda <- 10^seq(from = -1, to = 2, length = 1000)
+cts <- matrix(rpois(1000*100, lambda), ncol = 100)
+library("vsn")
+meanSdPlot(cts, ranks = FALSE)
+
+log.cts.one <- log2(cts + 1)
+meanSdPlot(log.cts.one, ranks = FALSE)
 
 
-dds <- DESeqDataSet(gse, design = ~ dex)
+
+dds <- DESeq(dds)
+res <- results(dds)
+res
+summary(res)
+mcols(res, use.names = TRUE)
+
+
+library("apeglm")
+resultsNames(dds)
+
+#results(dds, contrast = c("cell", "N061011", "N61311"))
+
+res <- results(dds, contrast=c("dex","trt","untrt"))
+summary(res)
+
+DESeq2::plotMA(res)
+
+res <- lfcShrink(dds, coef="dex_trt_vs_untrt", type="apeglm")
+DESeq2::plotMA(res, ylim = c(-5, 5))
+
+
+res.05 <- results(dds, alpha = 0.05)
+table(res.05$padj < 0.05)
+
+
+resLFC1 <- results(dds, lfcThreshold=1)
+table(resLFC1$padj < 0.1)
+
+
+
+
 
 
